66f5c6ef459ec20a88027fde2fed9b991615b63e
NiBienvenu <nicibienvenu@gmail.com>
2024-10-18 07:11:27 +0200

    commrnt buttom

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -71,9 +71,9 @@
                 <img src="{{ asset('images/logo.jpeg') }}" alt="Son Light Logo" style="border-radius: 10%" class="d-inline-block align-top">
                 {{-- Son Light --}}
             </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+            {{-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon"></span>
-            </button>
+            </button> --}}
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
