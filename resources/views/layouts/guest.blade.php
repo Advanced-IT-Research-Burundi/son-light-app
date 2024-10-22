d7af55dfba1c7863ff7176d752a5ae8ad8d9f505
Jean-Lionel <nijeanlionel@gmailcom>
2024-10-22 09:44:50 +0200

    adding

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -4,13 +4,11 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
         <title>{{ config('app.name', 'Laravel') }}</title>
-
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
+        <link rel="stylesheet" href="https://unpkg.com/tailwindcss@2.2.19/dist/tailwind.min.css" />
         <!-- Scripts -->
     </head>
     <body class="font-sans text-gray-900 antialiased">
