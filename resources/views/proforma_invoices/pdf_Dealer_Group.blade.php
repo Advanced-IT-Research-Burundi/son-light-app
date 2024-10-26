00a7fe862218dc9c9ad2c5b7a8608030f99fd65f
Leandre NIMPAGARITSE <leandrenimpagaritse22@gmail.com>
2024-10-26 14:06:20 +0200

    fixation de la facture proforma pour Dealer Group

--- resources/views/proforma_invoices/pdf_Dealer_Group.blade.php
+++ resources/views/proforma_invoices/pdf_Dealer_Group.blade.php
@@ -35,13 +35,13 @@
 
         .header_left {
             float: left;
-            padding-left: 5%;
+            padding-left: 8%;
         }
 
         .header_right {
             float: right;
             text-align: left;
-            padding-right: 30%;
+            padding-right: 35%;
         }
 
         .colored-bars {
@@ -81,7 +81,7 @@
 
         th, td {
             border: 1px solid black;
-            padding: 10px;
+            padding: 4px;
             text-align: left; 
         }
 
@@ -133,6 +133,20 @@
                 font-size: 0.9em; 
             }
         }
+          .vertical-barr{
+            height: 90px;
+            width: 3px;
+            transform: rotate(15deg);
+            border: 2px solid black;
+            float: left;
+            margin-right: 0px;
+            margin-top:50px;
+        }
+        .liste{
+            float: right;
+            margin-left: 0px;
+        }
+    
     </style>
 </head>
 <body>
@@ -144,14 +158,15 @@
             </strong></p>
         </div> 
         <div class="header_right">
-           <p>
-           <ul>
-                <li><strong><span style="color: blue;">=></span> Services TICs</strong></li>
-                <li><strong><span style="color: blue;">=></span> Agro Business</strong></li>
-                <li><strong><span style="color: blue;">=></span> Location Véhicule</strong></li>
-                <li><strong><span style="color: blue;">=></span> Commerce Général</strong></li>
-            </ul>  
-           </p>  
+       <div class="vertical-barr"></div>
+            <div class="liste">
+                <p><br></p>
+                <p style="padding:0;margin:0;"> &nbsp;&nbsp;&nbsp;&nbsp; <strong><span style="color: blue;">=></span>Services TICs</strong></p>
+                <p style="padding:0;margin:0;"> &nbsp;&nbsp;&nbsp;<strong><span style="color: blue;">=></span>Agro Business</strong></p>
+                <p  style="padding:0;margin:0;"> &nbsp;&nbsp;<strong><span style="color: blue;">=></span>Location Véhicule</strong></p>
+                <p  style="padding:0;margin:0;"><strong><span style="color: blue;">=></span> Commerce Général</strong></p>
+            
+         </div>  
         </div>  
     </div>
 
@@ -165,13 +180,13 @@
         <h4 class="border_header">FACTURE PROFORMA</h4>
         <h4 class="date">Date: Le {{ $proforma_invoice->created_at->format('d/m/Y') }}</h4>
     </div>
-    <p><br></p>  <p><br></p>
-    <h4 class="client-title"> <span style=" text-decoration: underline;">CLIENT </span>:{{ $proforma_invoice->client->name }}</h4>
+    <p><br></p><br>  <p><br></p>
+    <h4 class="client-title" style="padding:0;margin:0;"> <span style=" text-decoration: underline;">CLIENT </span>:{{ $proforma_invoice->client->name }}</h4>
     <div class="border-text">
         <table>
             <tr>
-                <th>ORDRE</th>
-                <th>Nature de l'article ou service</th>
+                <th>Ordre</th>
+                <th>Nature de l'article ou du service</th>
                 <th>Quantité</th>
                 <th>P.U en FBU</th>
                 <th>PVHTVA en FBU</th>
